bc3690678f65d51fdbd82c7cc7b74e07137fffdc
20120583 <trananhthi56@gmail.com>
2024-03-07 16:18:39 +0700

    fix outDir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          tailwindcss: ["tailwindcss"],
+        },
+      },
+    },
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
     },
